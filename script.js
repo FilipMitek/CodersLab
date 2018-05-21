daf3616f00ef9d8ba931de6f4fc7630e08b21f41
Filip Mitek <filip.mitek@gmail.com>
2018-05-22 00:54:10 +0200

    Zmienne i typy danych prework

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ console.log('Filip');
 // "Hello World"; shows Hello World
 // Hello World;  shows flt code uncaught SyntaxError: Unexpected identifier
 // "Hello World; shows Uncaught SyntaxError: Invalid or unexpected token
+
+var number = 7;         //To jest zmienna przechowująca liczbę  
+console.log(number);
+var strin = "7d";       //To jest zmienna przechowująca stringa  
+console.log(strin);
+var mix = 2 + "7d";     //To jest zmienna przechowująca mix liczby i stringa   
+console.log(mix);
+var bolean = true;      //To jest zmienna przechowująca wartosc bool  
+console.log(bolean);
+var special = undefined;//To jest zmienna przechowująca zmienną specjalną 
+console.log(undefined);
+var table = [2,"g",12]; //To jest zmienna przechowująca tablicę zmiennych
+console.log(table);
+
+var first = 123;
+var second = 127;
+var result = 0;
+var result = first + second ;
+console.log(result);
+
+var empty;
+console.log(empty); // Otrzymao niezdefiniowaną zmienna
