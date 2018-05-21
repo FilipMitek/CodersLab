23379336a517007e8b89c3c68d1eb6966e7819d9
Filip Mitek <filip.mitek@gmail.com>
2018-05-22 00:22:21 +0200

    Wykonano zadanie pierwszy skrypt

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log('Hellos World!'); // Tutaj wypiujemy tekst hello world 
-
+// Line under display your name
+console.log('Filip');
+//This line shows basic math operations
+// 2 + 3 shows 5
+// 10/4 shows 2.5
+// 30* 2 shows 60
+// "Hello World"; shows Hello World
+// Hello World;  shows flt code uncaught SyntaxError: Unexpected identifier
+// "Hello World; shows Uncaught SyntaxError: Invalid or unexpected token
