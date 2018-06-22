34f4a9b1f8cb55d49a4b7a4c8c1d34d11a01cd97
Filip Mitek <filip.mitek@gmail.com>
2018-06-22 21:32:12 +0200

    Zadania jScript wykonane

--- Prework/JScript/Zadanie_1/script/script.js
+++ Prework/JScript/Zadanie_1/script/script.js
@@ -1,1 +1,6 @@
 console.log("Filip Mitek");
+/*
+Po wpisaniu do konsoli Chrome "Hello World" tekst w apostrofie został przepisany.
+Po wpisaniu  Hello World; otrzymalismy komunikat o zlej skladni i niezidentyfikowany identyfikatorze
+Po wpisaniu "Hello World dostalismy info invalid or unexpected token, które odpowiada za niezidentyfikowane oznaczenie"
+*/
