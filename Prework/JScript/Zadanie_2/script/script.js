cbe9e7b3fc870c0969e057184c1be8d238b7b3c0
Filip Mitek <filip.mitek@gmail.com>
2018-06-26 18:08:33 +0200

    Zadanie numer 2 JavaScript gotowe

---
+++ Prework/JScript/Zadanie_2/script/script.js
@@ -0,0 +1,35 @@
+/*Zadanie numer 1
+*/
+
+var age = 24;
+var name = "Filip";
+var wartosc = false;
+var tablica = [2, 3, 5];
+
+console.log("Age : " + age);
+console.log("My name is : " +name);
+console.log("My Bools value : " +wartosc);
+console.log("My array : " +tablica);
+console.log("Filip");
+
+/*
+Zadanie numer 2
+*/
+
+var firstNumber = 20;
+var secondNumber = 21;
+var result = 0;
+
+
+result = firstNumber + secondNumber;
+console.log("Sume : "+result);
+
+
+/*
+Zadanie numer 3
+*/
+
+var empty ;
+
+console.log(empty); 
+// Zmienna wyświtlona w przeglądarce zostaje określona jako "undefined "
